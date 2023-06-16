127d097c03ff992bbfa5807294fe8a61e4be671c
Viacheslav <afonin.viacheslav@yandex.ru>
2023-06-16 15:55:07 +0300

    first repair after Lehrer

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ addBtn.addEventListener("click", ()=>{
 	let currentTaskN;
 	p.textContent=taskText;
 	//
-	btnDel=document.createElement("button");
+	const btnDel=document.createElement("button");
 	btnDel.textContent="delete";
 	btnDel.setAttribute("class", "buttonDel");
 	//
@@ -21,30 +21,33 @@ addBtn.addEventListener("click", ()=>{
 	curLi.append(btnDel);
 	container.append(curLi);
 	//
-	/*
+	//ce ne worked because I ne declared LET btnDel, niow works gut
 	btnDel.addEventListener("click", ()=>{
 		let p=btnDel.parentNode.querySelector("p");
 		let txt=p.textContent;
 		//
 		tasks=document.querySelectorAll("li");
 		countTasks=0;
+		if(tasks!=null)countTasks=tasks.length;
 		for(let j=1; j<=countTasks; j++){
-			if(tasks[j-1]==btnDel.parentNode){
+			if(tasks[j-1]===btnDel.parentNode){
 				currentTaskN=j;
 			}
 		}
-		if(tasks!=null)countTasks=tasks.length;
 		//
-		alert("You are deleting following task: "+txt+"(N"+currentTaskN+")");
+		alert("You are deleting following task: "+txt+"(N"+currentTaskN+" of "+countTasks+")");
 		btnDel.parentNode.remove();
 		//
-		if(countTasks==0){
+		//tasks=document.querySelectorAll("li");
+		//if(tasks!=null)countTasks=tasks.length;
+		//if(countTasks==0){
+		if(countTasks==0+1){
 			primaryText.removeAttribute("class", "sectionInvisible");
 		}
 	});
-	*/
-	//
 	
+	//
+	/*
 	//works, but warns not 1, but several times!
 	let delBtns=document.querySelectorAll(".buttonDel");
 	if(delBtns!=null){
@@ -71,6 +74,7 @@ addBtn.addEventListener("click", ()=>{
 			});
 		}
 	}
+	*/
 	//curLi.append(btnDel);
 	//container.append(curLi);
 	//
