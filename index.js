bdc8891a209bb2bc44caa5710fc0153e5508ad4a
Viacheslav <afonin.viacheslav@yandex.ru>
2023-06-20 23:16:38 +0300

    add ins up buttons and dn who is not done

--- index.js
+++ index.js
@@ -1,94 +1,241 @@
 //const addBtn=document.querySelector("#newTask");
 const addBtn=document.getElementById("newTask");
 const container=document.querySelector("ul");
-let tasks=document.querySelectorAll("li");
+let tasks=document.querySelector("li");
 let countTasks=0;
 if(tasks!=null)countTasks=tasks.length;
-//
-addBtn.addEventListener("click", ()=>{
-	//debugger;
-	const taskText=prompt("Task content:");
+//let curButtonDel=document.createElement("button");
+let primaryText=document.getElementById("PrimaryText");
+//primaryText.setAttribute("class", "sectionInvisible");
+
+//function generateBtnDel()
+
+function createTaskComponent(taskText){
+	//
 	let curLi=document.createElement("li");
-	let p=document.createElement("p");
-	let currentTaskN;
-	p.textContent=taskText;
+	curP=document.createElement("p");
+	curP.textContent=taskText;
 	//
 	const btnDel=document.createElement("button");
 	btnDel.textContent="delete";
-	btnDel.setAttribute("class", "buttonDel");
+	btnDel.setAttribute("class","buttonDel");
+	const btnIns=document.createElement("button");
+	btnIns.textContent="insert";
+	const btnUp=document.createElement("button");
+	btnUp.textContent="up";
+	const btnDn=document.createElement("button");
+	btnDn.textContent="down";
+	//btnIns.setAttribute("class","buttonIns");
 	//
-	curLi.append(p);
+	curLi.append(curP)
 	curLi.append(btnDel);
-	container.append(curLi);
+	curLi.append(btnIns);
+	curLi.append(btnUp);
+	curLi.append(btnDn);
 	//
-	//ce ne worked because I ne declared LET btnDel, niow works gut
+	let tasks=document.querySelectorAll('li');
+	
+	let countTasks=0;
+	if(tasks!=null){
+		countTasks=tasks.length;
+	}else alert("Nop tasks "+tasks);
+	//
 	btnDel.addEventListener("click", ()=>{
+		//
 		let p=btnDel.parentNode.querySelector("p");
 		let txt=p.textContent;
 		//
+		let countTasks=0, curTaskN=0;
+		tasks=document.querySelectorAll("li");
+		if(tasks!=null){
+			countTasks=tasks.length;
+		}
+		for(let i=1; i<=countTasks; i++){
+			if(tasks[i-1]===btnIns.parentNode){
+				curTaskN=i;
+				break;
+			}
+		}
+		alert("You are DELETING task: "+txt+" ("+curTaskN+")");
+		//
+		btnDel.parentNode.remove();
+	});
+	//
+	btnIns.addEventListener("click", ()=>{
+		//
+		let p=btnDel.parentNode.querySelector("p");
+		let txt=p.textContent;
+		//
+		let countTasks=0, curTaskN=0;
+		tasks=document.querySelectorAll("li");
+		if(tasks!=null){
+			countTasks=tasks.length;
+		}
+		for(let i=1; i<=countTasks; i++){
+			if(tasks[i-1]===btnIns.parentNode){
+				curTaskN=i;
+				break;
+			}
+		}
+		alert("You are inserting your new task before existing: "+txt+" ("+curTaskN+")");
+		//
+		const taskText=prompt("Task content:");
+		//
+		//btnIns.parentNode.insertBefore(createTaskComponent(taskText), null);
+		btnIns.parentNode.parentNode.insertBefore(createTaskComponent(taskText), btnIns.parentNode);
+	});
+	//
+	btnUp.addEventListener("click", ()=>{
+		//
+		let p=btnUp.parentNode.querySelector("p");
+		let txt=p.textContent;
+		//
+		let countTasks=0, curTaskN=0;
+		tasks=document.querySelectorAll("li");
+		if(tasks!=null){
+			countTasks=tasks.length;
+		}
+		for(let i=1; i<=countTasks; i++){
+			if(tasks[i-1]===btnUp.parentNode){
+				curTaskN=i;
+				break;
+			}
+		}
+		if(curTaskN>1){
+			btnUp.parentNode.parentNode.insertBefore(createTaskComponent(taskText), tasks[curTaskN-1-1]);
+			btnUp.parentNode.remove();
+		}
+	});
+	//
+	btnDn.addEventListener("click", ()=>{
+		//
+		let p=btnUp.parentNode.querySelector("p");
+		let txt=p.textContent;
+		//
+		let countTasks=0, curTaskN=0;
+		tasks=document.querySelectorAll("li");
+		if(tasks!=null){
+			countTasks=tasks.length;
+		}
+		for(let i=1; i<=countTasks; i++){
+			if(tasks[i-1]===btnUp.parentNode){
+				curTaskN=i;
+				break;
+			}
+		}
+		if(curTaskN<countTasks){
+			btnUp.parentNode.parentNode.insertAfter(createTaskComponent(taskText), tasks[curTaskN+1-1]);
+			btnUp.parentNode.remove();
+		}
+	});
+	return curLi;
+}
+
+addBtn.addEventListener("click", ()=>{
+	const taskText=prompt("Task content:");
+	container.append(createTaskComponent(taskText));
+});
+/*
+	let curLi=document.createElement("li");
+	curP=document.createElement("p");
+	curP.textContent=taskText;
+	//
+	const btnDel=document.createElement("button");
+	btnDel.textContent="delete";
+	btnDel.setAttribute("class","buttonDel");
+	//
+	const btnGetTaskN=document.createElement("button");
+	btnGetTaskN.textContent="GetTaskN";
+	//
+	curLi.append(curP);
+	curLi.append(btnDel);
+	curLi.append(btnGetTaskN);
+	//
+	container.append(curLi);
+	tnDel.addEventListener("click", () =>{
+		//let txt=btnDel.parentNode.textContent;//worked for simple li
 		tasks=document.querySelectorAll("li");
 		countTasks=0;
 		if(tasks!=null)countTasks=tasks.length;
+		//
+		let curTask1=btnDel.parentNode;
+		let curTaskN;
+		//
 		for(let j=1; j<=countTasks; j++){
-			if(tasks[j-1]===btnDel.parentNode){
-				currentTaskN=j;
+			if(tasks[j-1]==curTask1){
+				curTaskN=j;
 			}
 		}
 		//
-		alert("You are deleting following task: "+txt+"(N"+currentTaskN+" of "+countTasks+")");
+		let p=btnDel.parentNode.querySelector("p");
+		let txt=p.textContent;
+		alert("You are DELETING task: "+txt+" ("+curTaskN+")");
 		btnDel.parentNode.remove();
 		//
-		//tasks=document.querySelectorAll("li");
-		//if(tasks!=null)countTasks=tasks.length;
-		//if(countTasks==0){
-		if(countTasks==0+1){
+		if(countTasks==0){
+			primaryText.removeAttribute("class", "sectionInvisible");
+		}
+		//
+		tasks=document.querySelectorAll("li");
+		if(tasks!=null)countTasks=tasks.length;
+		if(countTasks==0){
 			primaryText.removeAttribute("class", "sectionInvisible");
 		}
 	});
-	
-	//
-	/*
-	//works, but warns not 1, but several times!
-	let delBtns=document.querySelectorAll(".buttonDel");
-	if(delBtns!=null){
-		for(let delBtn of delBtns){
-			delBtn.addEventListener("click", () =>{
-				let p=delBtn.parentNode.querySelector("p");
-				let txt=p.textContent;
-				//
-				tasks=document.querySelectorAll("li");
-				countTasks=0;
-				for(let j=1; j<=countTasks; j++){
-					if(tasks[j-1]==delBtn.parentNode){
-						currentTaskN=j;
-					}
-				}
-				if(tasks!=null)countTasks=tasks.length;
-				//
-				alert("You are deleting following task: "+txt+"(N"+currentTaskN+")");
-				delBtn.parentNode.remove();
-				//
-				if(countTasks==0){
-					primaryText.removeAttribute("class", "sectionInvisible");
-				}
-			});
+	btnGetTaskN.addEventListener("click", () =>{
+		const tasks=document.querySelectorAll('li');
+		const curTask=btnGetTaskN.parentNode;
+		const  newLi=document.createElement("li");
+		curP=document.createElement("p");
+		curP.textContent=taskText;
+		curLi.append(curP);
+		
+		for(let j=1; j<=countTasks; j++){
+			if(tasks[j-1]==curTask1){
+				curTaskN=j;
+				break;
+			}
 		}
-	}
-	*/
-	//curLi.append(btnDel);
-	//container.append(curLi);
+	});
 	//
 	let primaryText=document.getElementById("PrimaryText");
 	primaryText.setAttribute("class", "sectionInvisible");
-	//curLi.
-});
+	//
+	
+	//let delBtns=document.querySelectorAll(".buttonDel");
+	//if(delBtns!=null){
+	//	for(let delBtn of delBtns){
+	//		delBtn.addEventListener("click", () =>{
+	//			let p=delBtn.parentNode.querySelector("p");
+	//			let txt=p.textContent;
+	//			alert("You are DELETING task: "+txt);
+	//			delBtn.parentNode.remove();
+	//			//
+	//			tasks=document.querySelectorAll("li");
+	//			if(tasks!=null)countTasks=tasks.length;
+	//			if(countTasks==0){
+	//				primaryText.removeAttribute("class", "sectionInvisible");
+	//			}
+	//		});
+		}
+	//	
+	//}
+	//
+	
+//});
+*/
+
+
+
+
+
 /*
 let delBtns=document.querySelectorAll(".buttonDel");
 if(delBtns!=null){
+	//debugger;
 	for(delBtn of delBtns){
 		delBtn.addEventListener("click", () =>{
-			let taskToDel=delBtn.parentNode;
+			delBtn.parentNode.remove();
 		});
 	}
-}
-*/
+}*/
